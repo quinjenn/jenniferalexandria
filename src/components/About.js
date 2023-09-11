1c39a641ea845e50b92c8af713d7add9b8eabc41
quinjenn <jquintal_@hotmail.com>
2023-09-11 16:16:02 -0700

    about responsive

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,7 @@ const About = () => {
           width="36%"
         />
         <div className='bio-text-container'><p className='bio-text'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+          I'm an innovative Full Stack Developer with a passion for creative problem-solving. With a background in operations and experiential marketing, I am confident about providing aesthetic products and delivering refined solutions that enrich user experiences.
         </p>
         </div>
       </div>
