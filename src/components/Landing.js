9ac1b14f2ae733a39f207e1d72f7b87654eb9427
quinjenn <jquintal_@hotmail.com>
2024-10-26 21:01:29 -0700

    add animation landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -22,8 +22,6 @@ const Landing = () => {
           </p>
           <p className='landing-title-component2'>With Bespoke Website Solutions
           </p>
-          {/* <p className='landing-subtitle-component'>From innovative web design to the latest digital trends of the season. And more: insights from the tech world, intriguing web curiosities, top-tier developer tips, and expert guidance on web solutions.
-          </p> */}
         </div>
       </div>
     </div>
